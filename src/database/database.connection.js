4a2db0b6a98602db09e9e3eaaf65758a004e0bf2
Carlos Siqueri <carlossiqueri@Carlos>
2023-04-25 17:22:29 -0300

    mongoDB Atlas deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,15 +1,15 @@
-import { MongoClient } from "mongodb"
-import dotenv from "dotenv"
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // Database config
-const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const mongoClient = new MongoClient(process.env.DATABASE_URI);
 try {
-    await mongoClient.connect()
-    console.log("Mongodb connected");
-} catch (err){
-    console.log(err.message)
+  await mongoClient.connect();
+  console.log("Mongodb connected");
+} catch (err) {
+  console.log(err.message);
 }
 
 export const db = mongoClient.db();
