9c697587ee26b3285e71fdbf89a09b1d2713a85c
Carlos Siqueri <carlossiqueri@Carlos>
2023-04-25 17:03:02 -0300

    mongoDB Atlas deploy

--- src/app.js
+++ src/app.js
@@ -12,5 +12,5 @@ app.use(cors());
 app.use(router);
 
 // Server waiting for requests
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
